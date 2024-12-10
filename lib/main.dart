0c157e0d8f97321ad97c91c1ae231f18c6c1b981
Batu Battal <165045848+Batubttl@users.noreply.github.com>
2024-12-10 14:15:38 +0300

    Array Senaryoları

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(const MyApp());
 }
 
+void runApp(MyApp myApp) {}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,7 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -23,8 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
- 
 
   final String title;
 
@@ -37,14 +36,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-     
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -55,7 +52,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:neon_apps_full1/main.dart';
+
+// ignore: avoid_relative_lib_imports
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

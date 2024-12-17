f290c030c848f99398ce5e1d18324489c384583f
Batu Battal <165045848+Batubttl@users.noreply.github.com>
2024-12-17 13:17:57 +0300

    Container case completed

---
+++ lib/UI/Container.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+final String appName = "Awesome Gallery";
+
+class AwesomeGallery extends StatefulWidget {
+  const AwesomeGallery({super.key});
+
+  @override
+  State<AwesomeGallery> createState() => _AwesomeGalleryState();
+}
+
+final images = [
+  {'url': 'assets/sehir.jpg', 'title': 'Sehir'},
+  {'url': 'assets/dag.jpg', 'title': 'Dag'},
+  {'url': 'assets/okyanus.jpg', 'title': 'Okyanus'},
+  {'url': 'assets/doga.jpg', 'title': 'Doga'},
+];
+
+class _AwesomeGalleryState extends State<AwesomeGallery> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(appName),
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 2,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          final image = images[index];
+          return GestureDetector(
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('${image['title']}seçildi!')));
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3))
+                  ]),
+              child: Column(
+                children: [
+                  Expanded(
+                      child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      image['url']!,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                  )),
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                    child: Text(
+                      image['title']!,
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

0c1debebaaa276e10d168a651a20858fbc0d0c1c
Batu Battal <165045848+Batubttl@users.noreply.github.com>
2024-12-24 12:30:02 +0300

    Some UI cases completed

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:yeni_neonapps/sliders.dart';
+import 'package:yeni_neonapps/steppers.dart';
+import 'package:yeni_neonapps/switches.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData.dark().copyWith(
+            appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+          backgroundColor: Colors.purple,
+          elevation: 0,
+        )),
+        home: const StepperViewsApp());
+  }
+}
---
+++ lib/page_view.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class PageViewScreen extends StatefulWidget {
+  const PageViewScreen({super.key});
+
+  @override
+  State<PageViewScreen> createState() => _PageViewScreenState();
+}
+
+class _PageViewScreenState extends State<PageViewScreen> {
+  final List<Map<String, String>> _pages = [
+    {
+      "title": "Welcome the new Page",
+      "subtitle": "This page gonna be fun",
+      "image": "assets/adamsandler.jpeg"
+    },
+    {
+      "title": "Welcome the second Page",
+      "subtitle": "This page gonna be sad",
+      "image": "assets/İbiza.jpg"
+    },
+    {
+      "title": "Welcome the third Page",
+      "subtitle": "This page gonna be amazing",
+      "image": "assets/doga.jpg"
+    },
+    {
+      "title": "Welcome the third Page",
+      "subtitle": "This page gonna be dissapointed",
+      "image": "assets/FCork.jpeg"
+    },
+  ];
+
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Page View Example"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: PageView.builder(
+            controller: _pageController,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+              });
+            },
+            itemCount: _pages.length,
+            itemBuilder: (context, index) {
+              final page = _pages[index];
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    page["image"]!,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    page["title"]!,
+                    style: const TextStyle(
+                        fontStyle: FontStyle.normal, fontSize: 18),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    page["subtitle"]!,
+                    style: const TextStyle(
+                        fontStyle: FontStyle.italic, fontSize: 15),
+                  )
+                ],
+              );
+            },
+          )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                color: Colors.red,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
